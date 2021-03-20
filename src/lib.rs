4c5320e4f41d8ce07d92f7bac8cda3319fdd03f2
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2021-03-20 19:25:44 +0100

    Add more parsing

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{collections::HashMap, fmt};
-use pest::iterators::Pairs;
+use pest::{error::ErrorVariant, iterators::{Pair, Pairs}};
 
 #[macro_use]
 extern crate pest;
@@ -15,14 +15,26 @@ struct AsklParser;
 type Params<'i> = HashMap<&'i str, Rule>;
 
 pub struct Askl<'i> {
-    params: Params<'i>,
-    pairs: Pairs<'i, Rule>
+    params: Vec<Pair<'i, Rule>>,
+    find: Vec<Pair<'i, Rule>>,
+    command: Pair<'i, Rule>,
+    // pairs: Pairs<'i, Rule>
 }
 
 #[derive(Debug)]
 pub struct AskError {
     pest_err: pest::error::Error<Rule>
 }
+
+impl AskError {
+    fn new(message: &str, span: pest::Span) -> AskError {
+        AskError{
+            pest_err: pest::error::Error::new_from_span(
+                ErrorVariant::CustomError{message: message.to_string()}, span)
+        }
+    }
+}
+type Result<T> = std::result::Result<T, AskError>;
 
 impl fmt::Display for AskError {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
@@ -31,18 +43,45 @@ impl fmt::Display for AskError {
 }
 
 impl<'i> Askl<'i> {
-    fn extract_params(pairs: Pairs<'i, Rule>) -> (Pairs<'i, Rule>, Params) {
-        (pairs, HashMap::new())
+    fn extract(pairs: Pairs<'i, Rule>) -> Result<Askl> {
+        let mut params = Vec::<Pair<'i, Rule>>::new();
+        let mut find = Vec::<Pair<'i, Rule>>::new();
+        let mut command = Vec::<Pair::<'i, Rule>>::new();
+        pairs.for_each(|pair| {
+            println!("{:#?}", pair);
+            match pair.as_rule() {
+                Rule::param => {
+                    params.push(pair);
+                },
+                Rule::find => {
+                    find.push(pair)
+                },
+                Rule::operation => {
+                    command.push(pair)
+                }
+                Rule::EOI => (),
+                _ => unreachable!(),
+            };
+        });
+
+        if command.len() > 1 {
+            let first_command = command.first().unwrap().as_span();
+            let (l, c) = first_command.start_pos().line_col();
+            println!("More than one outer command defined. Previous defined at line {} column {}: \n{}",
+                     l, c, first_command.start_pos().line_of());
+            return Err(AskError::new("Operation redefined", first_command));
+        }
+        Ok(Askl{params, find, command:command.pop().unwrap()})
     }
 
-    pub fn new(ask_code: &str) -> Result<Askl, AskError> {
+    pub fn new(ask_code: &str) -> Result<Askl> {
         println!("Parsing: {}", ask_code);
         match AsklParser::parse(Rule::ask, ask_code) {
-            Ok(mut pairs) => {
+            Ok(pairs) => {
                 println!("{:#?}", pairs);
 
-                let (pairs, params) = Askl::extract_params(pairs);
-                Ok(Askl{params, pairs})
+                let askl = Askl::extract(pairs);
+                askl
             },
             Err(error) => {
                 println!("Error: {}", error);
