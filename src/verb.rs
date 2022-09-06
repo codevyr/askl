6a27a90f736ffeb80eea94ec87b52bd381c64692
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2022-09-06 10:23:52 +0200

    Require name field in the filter verb

--- src/verb.rs
+++ src/verb.rs
@@ -1,5 +1,6 @@
 use crate::cfg::ControlFlowGraph;
-use crate::parser::{ Identifier, NamedArgument, Rule};
+use crate::parser::{Identifier, NamedArgument, Rule};
+use anyhow::{bail, Result, anyhow};
 use core::fmt::Debug;
 use pest::error::Error;
 use std::collections::HashMap;
@@ -7,7 +8,9 @@ use std::collections::HashMap;
 pub fn build_verb(pair: pest::iterators::Pair<Rule>) -> Result<Box<dyn Verb>, Error<Rule>> {
     let mut pair = pair.into_inner();
     let ident = pair.next().unwrap();
-    let args = pair.map(NamedArgument::build).collect::<Result<Vec<_>, _>>()?;
+    let args = pair
+        .map(NamedArgument::build)
+        .collect::<Result<Vec<_>, _>>()?;
 
     let positional = vec![];
     let mut named = HashMap::new();
@@ -17,14 +20,17 @@ pub fn build_verb(pair: pest::iterators::Pair<Rule>) -> Result<Box<dyn Verb>, Er
 
     let span = ident.as_span();
     match Identifier::build(ident)?.0.as_str() {
-        FilterVerb::NAME => Ok(Box::new(FilterVerb::new(positional, named))),
-        unknown => Err(Error::new_from_span(
+        FilterVerb::NAME => FilterVerb::new(positional, named),
+        unknown => Err(anyhow!("Unknown filter: {}", unknown)),
+    }
+    .map_err(|e| {
+        Error::new_from_span(
             pest::error::ErrorVariant::CustomError {
-                message: format!("Unknown filter: {}", unknown),
+                message: format!("Failed to create filter: {}", e),
             },
             span,
-        ))?,
-    }
+        )
+    })
 }
 
 pub trait Verb: Debug {
@@ -40,8 +46,12 @@ struct FilterVerb {
 impl FilterVerb {
     const NAME: &'static str = "filter";
 
-    fn new(positional: Vec<String>, named: HashMap<String, String>) -> Self {
-        Self { positional, named }
+    fn new(positional: Vec<String>, named: HashMap<String, String>) -> Result<Box<dyn Verb>> {
+        if !named.contains_key("name") {
+            bail!("Must contain name field");
+        }
+
+        Ok(Box::new(Self { positional, named }))
     }
 }
 
