883e6314f4f5298b7dd85212ffac916a5ed36463
Maksym Planeta <mplaneta@os.inf.tu-dresden.de>
2023-08-19 17:05:15 +0200

    Add progress indication

--- src/bin/askl/main.rs
+++ src/bin/askl/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
 use env_logger;
+use console::{style, Emoji};
 use petgraph::dot::{Config, Dot};
 
 use askl::executor::Executor;
@@ -20,17 +21,42 @@ struct Args {
     query: String,
 }
 
+static LOOKING_GLASS: Emoji<'_, '_> = Emoji("🔍  ", "");
+static SPARKLE: Emoji<'_, '_> = Emoji("✨ ", ":-)");
+static CLIP: Emoji<'_, '_> = Emoji("🔗  ", "");
+static PAPER: Emoji<'_, '_> = Emoji("📃  ", "");
+
 fn main() -> Result<()> {
     env_logger::init();
     let args = Args::parse();
 
+    println!(
+        "{} {}Loading index...",
+        style("[1/4]").bold().dim(),
+        LOOKING_GLASS
+    );
     let symbols: SymbolMap = serde_json::from_slice(&std::fs::read(args.index)?)?;
 
+    println!(
+        "{} {}Parsing query...",
+        style("[2/4]").bold().dim(),
+        SPARKLE
+    );
     let ast = parse(&args.query)?;
 
+    println!(
+        "{} {}Adding symbols...",
+        style("[3/4]").bold().dim(),
+        CLIP
+    );
     let mut executor = Executor::new(ast)?;
     executor.add_symbols(symbols);
 
+    println!(
+        "{} {}Running query...",
+        style("[4/4]").bold().dim(),
+        PAPER
+    );
     let cfg = executor.run();
     println!("{:?}", Dot::with_config(&cfg, &[Config::EdgeNoLabel]));
 
