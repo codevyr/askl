08024538c0bb861aae351aa9440eca1e9e60ae77
Maksym Planeta <mcsim.planeta@gmail.com>
2024-04-30 15:03:29 +0200

    Remove petgraph

--- src/cfg.rs
+++ src/cfg.rs
@@ -1,7 +1,6 @@
 use std::iter::Iterator;
 
 use crate::symbols::{Symbol, SymbolId, SymbolMap, Occurence};
-use petgraph::graphmap::DiGraphMap;
 
 #[derive(Debug, Clone)]
 pub struct ControlFlowGraph {
@@ -22,12 +21,6 @@ impl EdgeList {
 
 impl ControlFlowGraph {
     pub fn from_symbols(symbols: SymbolMap) -> Self {
-        let mut g = DiGraphMap::new();
-        for (parent_l, s) in symbols.iter() {
-            for child_l in s.children.iter() {
-                g.add_edge(parent_l, &child_l.id, ());
-            }
-        }
         Self {
             symbols: symbols,
         }
